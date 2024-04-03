6dbe59ed78d4e33703ec9be2f187a0340a672689
root <mgiraudo@ies21.edu.ar>
2024-04-03 20:23:03 -0300

    Dibujar firma sobre formulario "frmFirma".

---
+++ pryEjemploGit/frmFirma.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace pryEjemploGit
+{
+    partial class frmFirma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmFirma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmFirma";
+            this.Text = "frmFirma";
+            this.Load += new System.EventHandler(this.frmFirma_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.frmFirma_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmFirma_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ pryEjemploGit/frmFirma.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEjemploGit
+{
+    public partial class frmFirma : Form
+    {
+        public frmFirma()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFirma_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmFirma_MouseMove(object sender, MouseEventArgs e)
+        {
+            // this.Text = e.Location.ToString();
+        }
+
+        private void frmFirma_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Text = "Presiona el botón izquierdo";
+            }
+            else
+            {
+                this.Text = "-";
+            }
+        }
+    }
+}
